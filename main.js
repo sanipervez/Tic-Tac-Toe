e93e3d1c629075dcbd307f756f01a7a45a134c9e
Akash Gupta <akashgp9@gmail.com>
2020-04-02 21:01:04 +0530

    Added Comments to Index.Html,style.css & main.js file.Added The Draw logic in Js file.

--- main.js
+++ main.js
@@ -1,28 +1,41 @@
+//step is used in a logic that helps user to insert alternate images(cross,nought)
 let step=1;
+
+//pos helps to trace which box is clicked.If cross is the img display on box the pos will be alligned with value 1 or else pos=0
 let pos1=2,pos2=2;pos=3,pos3=66,pos4=7,pos5=9,pos6=11,pos7=22,pos8=223,pos9=44;
 
+//startGame is function that initiates the Game
+function startGame(){
+
+//winner Function determines which player has won the Game or is it a Draw
 function winner(val){
   if((pos1===pos2 && pos2===pos3) || (pos1===pos5 && pos5===pos9) || (pos7===pos5 && pos5===pos3) || (pos7===pos8 && pos8===pos9) || (pos1===pos4 && pos4===pos7) || (pos4===pos5 && pos5===pos6) || (pos2===pos5 && pos5===pos8) || (pos3===pos6 && pos6===pos9)){
     
     if(val==='cross'){
-      document.getElementById('button').innerHTML='<h4>Player <img class="button-img" src="./1-16405_american-red-cross-computer-icons-christian-cross-symbol.png"> Wins! Play Again<h4>';
+      //displays message on the button,player1 wins
+      document.getElementById('button').innerHTML='<h3>Player <img class="button-img" src="./1-16405_american-red-cross-computer-icons-christian-cross-symbol.png"> Wins! Play Again</h3>';
 
   
     }
     else{
-      document.getElementById('button').innerHTML='<h4>Player <img class="button-img"  src="./421-4211837_fond-colors-circle-poster-u37711-transparent-red-strikethrough.png"> Wins! Play Again<h4>';
+       //displays message on the button,player2 wins
+      document.getElementById('button').innerHTML='<h3>Player <img class="button-img"  src="./421-4211837_fond-colors-circle-poster-u37711-transparent-red-strikethrough.png"> Wins! Play Again</h3>';
       
     }
      element1.onclick=null; element2.onclick=null; element3.onclick=null; element4.onclick=null; element5.onclick=null; element6.onclick=null; element7.onclick=null; element8.onclick=null; element9.onclick=null;
   
   }
+   //displays message on the button,that the match is draw
+  else if(step===10){
+    document.getElementById('button').innerHTML="<h3>It's a DRAW! Play Again</h3>";
+  }
 
 }
 
-
+//all the box are targeted with different target variables,and onclick either the cross or nought is displayed on the boxes
 let element1=document.getElementById('box1');
 element1.onclick=()=>{
-  element1.onclick=null;
+  element1.onclick=null; //ensures that when the targetVariable(Here element1) when clicked again will have no affect 
   if(step%2!==0){
   element1.innerHTML='<img class="cross" src="./1-16405_american-red-cross-computer-icons-christian-cross-symbol.png">';
   step++;
@@ -175,4 +188,10 @@ pos9=0;
 winner('no');
 }
 }
-;
+}
+startGame();
+//Restarts the Game By refresing the window
+document.getElementById('button').onclick=()=>{
+  location = window.location;
+  startGame();
+}
